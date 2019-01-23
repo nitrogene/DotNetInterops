e57f8f3917981e287a07606ab3b17cdad5493a19
Jean-Pierre GERVASONI <jean.pierre.gervasoni@gmail.com>
2019-01-23 20:52:20 +0100

    When Hresult is not OK, display error string

--- CustomCOMObjectClient/CustomCOMObjectClient.cpp
+++ CustomCOMObjectClient/CustomCOMObjectClient.cpp
@@ -1,8 +1,11 @@
 #include <comutil.h>
 #include <iostream>
+#include <string>
+
 // To set the path to the folder containing the tlb, fo to project 
 // settings VC++ Directories/Library Directories
 #import "CustomCOMObject.tlb" named_guids
+
 
 // https ://alala666888.wordpress.com/2010/08/31/c-com-server-and-c-com-client-simple-sample/
 int main(int argc, char** argv)
@@ -15,7 +18,11 @@ int main(int argc, char** argv)
 		auto hRes = pSaySomething.CreateInstance(__uuidof(CustomCOMObject::SaySomething));
 		if (FAILED(hRes))
 		{
-			std::cout << "ISaySomethingPtr::CreateInstance failed:" << hRes << std::endl;
+			_com_error err(hRes);
+			LPCTSTR errMsg = err.ErrorMessage();
+
+
+			std::wcout << L"ISaySomethingPtr::CreateInstance failed:" << std::wstring(errMsg) << std::endl;
 		}
 		else
 		{
